1de0f66a96b8633ad07ea828e634d8d99084b74a
Omar <47008591+6f6d6172@users.noreply.github.com>
2026-01-02 16:32:15 -0500

    implicit persist credentials

---
+++ spec/claws/rule/implicit_persist_credentials_spec.rb
@@ -0,0 +1,47 @@
+RSpec.describe Claws::Rule::ImplicitPersistCredentials do
+  before do
+    load_detection
+  end
+
+  context "with default configuration" do
+    it "flags a checkout with no explicit setting for persist-credentials" do
+      violations = analyze(<<~YAML)
+        name: Check out the current repository
+
+        on: [pull_request]
+
+        jobs:
+          build:
+            steps:
+            - uses: actions/checkout@v6
+            - run: |
+                rake setup
+                rake spec
+      YAML
+
+      expect(violations.count).to eq(1)
+      expect(violations[0].line).to eq(8)
+      expect(violations[0].name).to eq("ImplicitPersistCredentials")
+    end
+
+    it "doesn't flag a checkout if persist-credentials is set to false" do
+      violations = analyze(<<~YAML)
+        name: Check out the current repository
+
+        on: [pull_request]
+
+        jobs:
+          build:
+            steps:
+            - uses: actions/checkout@v6
+              with:
+                persist-credentials: false
+            - run: |
+                rake setup
+                rake spec
+      YAML
+
+      expect(violations.count).to eq(0)
+    end
+  end
+end
