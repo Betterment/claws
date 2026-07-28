ea6a69e39b8c1272d70d9b9dce70224406ebec28
cosmosbit <194725413+cosmosbit@users.noreply.github.com>
2026-07-28 11:05:57 -0600

    feat: add support for environment variables in AWS static credentials configuration

--- lib/claws/rule/configure_aws_static_credentials.rb
+++ lib/claws/rule/configure_aws_static_credentials.rb
@@ -12,8 +12,18 @@ module Claws
         https://github.com/betterment/claws/blob/main/README.md#configureawsstaticcredentials
       DESC
 
-      on_workflow :flag_workflow_aws_access_key_env
-      on_workflow :flag_workflow_aws_secret_key_env
+      on_workflow %(
+        get_key($workflow.env, "AWS_ACCESS_KEY_ID") =~ "{{.*secrets\..*" ||
+        get_key($workflow.env, "AWS_ACCESS_KEY_ID") =~ "{{.*env\..*" ||
+        get_key($workflow.env, "AWS_ACCESS_KEY_ID") =~ "{{.*vars\..*" ||
+        get_key($workflow.env, "AWS_ACCESS_KEY_ID") =~ "AKIA.*"
+      ), highlight: "env.AWS_ACCESS_KEY_ID"
+
+      on_workflow %(
+        get_key($workflow.env, "AWS_SECRET_ACCESS_KEY") =~ "{{.*secrets\..*" ||
+        get_key($workflow.env, "AWS_SECRET_ACCESS_KEY") =~ "{{.*env\..*" ||
+        get_key($workflow.env, "AWS_SECRET_ACCESS_KEY") =~ "{{.*vars\..*"
+      ), highlight: "env.AWS_SECRET_ACCESS_KEY"
 
       on_job %(
         get_key($job.env, "AWS_ACCESS_KEY_ID") =~ "{{.*secrets\..*" ||
@@ -73,50 +83,6 @@ module Claws
         $step.run =~ ".*GITHUB_ENV.*AWS_SECRET_ACCESS_KEY.*(\{\{.*secrets\.|\{\{.*vars\.|\{\{.*env\.)" ||
         $step.run =~ ".*aws configure set aws_secret_access_key.*(\{\{.*secrets\.|\{\{.*vars\.|\{\{.*env\.)"
       ), highlight: "run"
-
-      def flag_workflow_aws_access_key_env(workflow:, job:, step:) # rubocop:disable Lint/UnusedMethodArgument
-        flag_workflow_env_var(workflow, "AWS_ACCESS_KEY_ID", include_akia: true)
-      end
-
-      def flag_workflow_aws_secret_key_env(workflow:, job:, step:) # rubocop:disable Lint/UnusedMethodArgument
-        flag_workflow_env_var(workflow, "AWS_SECRET_ACCESS_KEY", include_akia: false)
-      end
-
-      private
-
-      def flag_workflow_env_var(workflow, var_name, include_akia:)
-        data = workflow_data(workflow)
-        env_key = data.keys.find { |k| k.to_s == "env" }
-        return unless env_key
-
-        env = data[env_key]
-        return unless env.is_a? Hash
-
-        var_key = env.keys.find { |k| k.to_s == var_name }
-        return unless var_key
-
-        value = env[var_key]
-        return unless static_aws_credential_value?(value, include_akia:)
-
-        Violation.new(
-          line: var_key.line,
-          description:
-        )
-      end
-
-      def workflow_data(workflow)
-        workflow.instance_variable_get(:@workflow)
-      end
-
-      def static_aws_credential_value?(value, include_akia: false)
-        return false unless value.is_a? String
-
-        return true if include_akia && value =~ /AKIA/
-
-        value =~ /\{\{.*secrets\./ ||
-          value =~ /\{\{.*env\./ ||
-          value =~ /\{\{.*vars\./
-      end
     end
   end
 end
